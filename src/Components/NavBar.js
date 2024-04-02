2d9078fbd6451a8627eae0f5f0ef0b5c48db7d5b
adarsh.mahendrakar <you@example.com>
2024-04-03 02:15:06 +0530

    changed a tag to Link tag

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -41,10 +41,10 @@ function NavBar() {
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             <h3 className="nav-head-name">Adarsh Mahendrakar</h3>
             <button className="btn">Front-End Developer</button>
-          </a>
+          </Link>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav ml-auto">
               <li className="nav-item">
