3072b1665de457f4e580016cc788b74011bd1a3b
adarsh.mahendrakar <you@example.com>
2024-04-03 02:20:12 +0530

    removed unwanted imports

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,6 +1,5 @@
 import NavBar from "../Components/NavBar";
 import React from "react";
-import adarsh_portfolio_img from "../img/adarsh-portfolio.png";
 import { Experience } from "../Components/Experience";
 import { Skills } from "../Components/Skills";
 import { ReachMe } from "../Components/ReachMe";
